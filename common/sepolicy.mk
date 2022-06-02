d89db3fc975fd2b40d3ae32711004009d5ee04d2
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-06-02 10:23:10 +0530

    sepolicy: Refactor for CAF
    - Cleanup unneeded sepolicy
    - Transfer power service from vendor/blaster/sepolicy
    - Add additional sepolicy for updater

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -10,25 +10,17 @@ endif
 endif
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/aosp/sepolicy/common/public
+    device/blaster/sepolicy/common/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/aosp/sepolicy/common/private
+    device/blaster/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/aosp/sepolicy/common/dynamic \
-    device/aosp/sepolicy/common/system
+    device/blaster/sepolicy/common/dynamic \
+    device/blaster/sepolicy/common/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/aosp/sepolicy/common/dynamic \
-    device/aosp/sepolicy/common/vendor
-endif
-
-# Selectively include legacy rules defined by the products
--include device/aosp/sepolicy/legacy-common/sepolicy.mk
-
-# Include atv rules on atv product
-ifeq ($(PRODUCT_IS_ATV), true)
-include device/aosp/sepolicy/atv/sepolicy.mk
+    device/blaster/sepolicy/common/dynamic \
+    device/blaster/sepolicy/common/vendor
 endif
